f2a293100137e468d8155954fb94a483964d79d7
Damiana-Arangio <arangio.damiana@gmail.com>
2025-09-13 00:47:02 +0200

    snack-6 completato

--- snack-6/script.js
+++ snack-6/script.js
@@ -11,4 +11,15 @@ const zucchine = [
   { type: 'Calabrese', weight: 14, length: 4 },
 ];
 
-// A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+// A partire dall'array fornito, crea due array. 
+// Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+// Zucchine con lunghezza >= 15
+const arrayZucchineLunghe = zucchine.filter((zucchina) => zucchina.length >= 15);
+console.log("Zucchine lunghe: ", arrayZucchineLunghe)
+
+// Altre zucchine >= 15
+const arrayZucchineRimanenti = zucchine.filter((zucchina) => zucchine.length < 15 );
+console.log("Altre zucchine: ", arrayZucchineRimanenti)
+
+
